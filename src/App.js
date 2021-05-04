03ca49df32e056a98706f543ae6ac31d50b5b887
MayElectric <42119515+MayElectric@users.noreply.github.com>
2021-05-04 18:28:40 +0200

    Update: adding Alt Atr to Logo

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <section className="App">
       <div className="container pt-5 px-5 d-flex justify-content-end">
-        <img src={logo} width="50" height="50" />
+        <img src={logo} width="50" height="50" alt="design-mai-logo" />
       </div>
       <div className="container p-5">
         <Dictionary />
